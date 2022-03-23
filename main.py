8c0af3964a438f365ef2fb4a719b06d3884b40d9
GrishaTS <69619529+GrishaTS@users.noreply.github.com>
2022-03-24 01:07:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import schedule
 import datetime
 import parcer
 import json
+import random
 
 bot = telebot.TeleBot('1868908140:AAFU1Xeh3EFz5BKTvdDy4a5rHUwMrZxcoY0')
 
-
+Efremov = ['CAACAgIAAxkBAAIF1WI7k-69I8AqNMUEAcCI2YUkkwXnAAIUKwAC4KOCB9LIZOk_oYwwIwQ', 'CAACAgIAAxkBAAIF1mI7k-8k3LJxXlM9D_Fa6NfjdteLAAIVKwAC4KOCB5zLy1d58Hm7IwQ', 'CAACAgIAAxkBAAIF12I7k_DOwtp3E6XKFtVKedwyti1aAAIWKwAC4KOCB6VK7oULYB7VIwQ', 'CAACAgIAAxkBAAIF2GI7k_HaX_lKzwrI7iUrzEZ68Ty-AAIXKwAC4KOCB3A4tS5ZHSazIwQ', 'CAACAgIAAxkBAAIF2WI7k_Ljw1NIqfhAYqZmMQSlwjgjAAIYKwAC4KOCB230ikNVTe9EIwQ', 'CAACAgIAAxkBAAIF2mI7k_MnGi0ysLMQpi152STvDQuQAAIZKwAC4KOCB8PWNM1xrjJSIwQ', 'CAACAgIAAxkBAAIF22I7k_WrgfY7Dv7Sf6vgkSHr8vsDAAIcKwAC4KOCB4x_OaEtX2qqIwQ', 'CAACAgIAAxkBAAIF3GI7k_edCVCXxhf_Z7je-RhIsGXdAAIfKwAC4KOCBzOT8iv10acNIwQ', 'CAACAgIAAxkBAAIF3WI7lEKgfYDZ_91Y5XN_AQ4v0qcfAAK-awAC4KOCB43ZT50M7DHqIwQ', 'CAACAgIAAxkBAAIF3mI7lEOz-hh2C2zqCobII5tJ7VzuAAK_awAC4KOCB-pj81RwQk96IwQ', 'CAACAgIAAxkBAAIF32I7lETqTo_WXFPhUPELmoBOTCCtAALAawAC4KOCBxJDRKez-cFAIwQ', 'CAACAgIAAxkBAAIF4GI7lEVcYJ9-WiEr_d-v0uq8fKKuAALBawAC4KOCB36m8ggebDPBIwQ', 'CAACAgIAAxkBAAIF4WI7lEZXcYKBOsCdDImgrpriORFgAALCawAC4KOCBwYcn2HfpRXsIwQ', 'CAACAgIAAxkBAAIF4mI7lEZfELnrjwIoVixoWX1Iv9TsAALDawAC4KOCB698xgjx609XIwQ', 'CAACAgIAAxkBAAIF42I7lEuh_pzt-ejoCkJCPdxjREsZAALSawAC4KOCB-Oc6nSwIUuTIwQ', 'CAACAgIAAxkBAAIF5GI7lEvn4K8DZSQnYQxsN0XDH4TPAALRawAC4KOCBwMvb9X2NYZuIwQ', 'CAACAgIAAxkBAAIF5WI7lEyPJ7qF3dOQPFAmfODQPxtMAALOawAC4KOCB9KPgR9jGdlPIwQ', 'CAACAgIAAxkBAAIF5mI7lE105g14iPLEjZlkEKFg670iAALQawAC4KOCBxLIUnIdTiKwIwQ', 'CAACAgIAAxkBAAIF52I7lE1_a3_ExNLvV5FglKRSLEq4AALLawAC4KOCB6vsjdaatEAJIwQ']
 # noinspection PyMethodParameters
 
 
@@ -44,7 +45,6 @@ def check():
     for _id in database.keys():
         try:
             nowtime = OurTime(int(database[_id]['timez']))
-            print(nowtime.minute, nowtime.hour)
             dt = database[_id]['dtime']
             nowtime.minute += dt
             if nowtime.minute < 0:
@@ -85,6 +85,8 @@ def start(message):
     gif = \
         'vgifbot.online/gif/BAACAgIAAxkBAAFHeqJiOjZILiGb0tPsqgoVy9cRv3dQvQACvxgAAnJb0UlZOyxguHTiIiME_1647982227.79.gif'
     bot.send_message(message.chat.id, f'Привет, <b>{message.from_user.first_name}</b>', parse_mode='html')
+    if message.chat.id == 512770440:
+        bot.send_sticker(message.from_user.id, random.choice(Efremov))
     bot.send_animation(message.chat.id, gif)
     bot.send_message(message.chat.id, "Скинь боту Execel файл с сайта эжд https://school.mos.ru/")
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -97,6 +99,8 @@ def start(message):
 @bot.message_handler(commands=['dtime'])
 def setdtime(message):
     a = message.text.replace('/dtime ', '')
+    if message.chat.id == 512770440:
+        bot.send_sticker(message.from_user.id, random.choice(Efremov))
     try:
         dtime = int(a) - int(a) % 5
         if dtime <= 0 and int(a) != 0:
@@ -120,6 +124,11 @@ def setdtime(message):
 
 @bot.message_handler()
 def text(message):
+    if message.chat.id == 512770440:
+        bot.send_sticker(message.from_user.id, random.choice(Efremov))
+    today = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].index(
+        time.strftime("%A")) + 1
+    timing = time.strftime("%H:%M")
     if message.text == 'Часовой пояc':
         bot.send_message(message.chat.id, 'отправь сообщение в формате "+- х"', reply_markup=telebot.types.ReplyKeyboardRemove())
 
@@ -127,17 +136,37 @@ def text(message):
         timez = message.text.replace(" ", "").replace('+', '')
         parcer.change_tz(_id=message.chat.id,
                          newtz=timez)
-        bot.send_message(message.chat.id, "За сколько до урока скидывать уведомление(число кратное 5)?"
-                                          " Напишите команду /dtime x")
+        bot.send_message(message.chat.id, f"За сколько до урока скидывать уведомление(число кратное 5)?\nНапишите команду /dtime <b>{'x'}</b>", parse_mode="html")
     elif message.text == "Я из Москвы":
         timez = "3"
         parcer.change_tz(_id=message.chat.id,
                          newtz=timez)
         bot.send_message(message.chat.id, f"За сколько до урока скидывать уведомление(число кратное 5)?\nНапишите команду /dtime <b>{'x'}</b>", parse_mode='html', reply_markup=telebot.types.ReplyKeyboardRemove())
+    elif message.text == "Расписание на сегодня":
+        info = parcer.day_dairy(message.chat.id, today)
+        bot.send_message(message.chat.id, f'Сегодня{info}' if info == " нет уроков" else info)
+    elif message.text == "Расписание на завтра":
+        info = parcer.day_dairy(message.chat.id, (today + 1) % 7 if today != 6 else 7)
+        bot.send_message(message.chat.id, f'Завтра{info}' if info == " нет уроков" else info)
+    elif message.text == "Следующий урок":
+        with open("database.json") as file:
+            sfile = json.load(file)
+        next_today = 0 + today
+        info = ""
+        while info == "":
+            lessons = sfile[str(message.chat.id)][str(next_today)]
+            for i in lessons.keys():
+                if next_today != today or int(i.split(":")[0]) > int(timing.split(":")[0]) or (int(i.split(":")[0]) == int(timing.split(":")[0]) and int(i.split(":")[1]) > int(timing.split(":")[1])):
+                    info = f'Следующий урок в {["пн", "вт", "ср", "чт", "пт", "сб", "вс"][next_today - 1]} в {i}\n{lessons[i]}'
+                    break
+            next_today = (next_today + 1) % 7 if today != 6 else 7
+        bot.send_message(message.chat.id, info)
 
 
 @bot.message_handler(content_types=['document'])
 def handle_docs_photo(message):
+    if message.chat.id == 512770440:
+        bot.send_sticker(message.from_user.id, random.choice(Efremov))
     try:
         chat_id = message.chat.id
         file_info = bot.get_file(message.document.file_id)
@@ -163,7 +192,10 @@ def handle_docs_photo(message):
 
 @bot.message_handler()
 def send_not(_id, lesson, t):
-    bot.send_message(_id, f'Скоро урок "{lesson}"!\nНюхай бебру сынуля, время {t}')
+    with open("database.json") as file:
+        sfile = json.load(file)
+    dtime = sfile[str(_id)]["dtime"]
+    bot.send_message(_id, f'Скоро урок "{lesson}"!\nУрок через {dtime} минут')
 
 
 class ScheduleMessage:
